91974c687a7f447e33fba9ee8bf2d7fc97c3a2dd
mattias0h <hintmattias@gmail.com>
2015-12-01 12:45:52 +0200

    Lecture 26: Snake Game Scripting - Part B [#108704152]

--- snake/script.js
+++ snake/script.js
@@ -1,13 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //Define vars
-   var canvas = $('#canvas')[0];
+    var canvas = $('#canvas')[0];
     var ctx = canvas.getContext("2d");
-    var w = canvas.width();
-    var h = canvas.height();
+    var w = canvas.width;
+    var h = canvas.height;
     var cw = 15;
     var d = "right";
     var food;
     var score;
+    var color = "green";
     var speed = 130;
 
     //Snake array
@@ -30,7 +31,7 @@ $(document).ready(function () {
         var length = 5;
         snake_array = [];
         for(var i = length-1;i >= 0;i--){
-            snake_array.push({x: i,y:0});
+            snake_array.push({x: i,y: 0});
         }
     }
 
@@ -38,7 +39,68 @@ $(document).ready(function () {
     function create_food() {
         food = {
             x:Math.round(Math.random()*(w-cw)/cw),
-            y:Math.round(Math.random()*(h-cw)/cw);
+            y:Math.round(Math.random()*(h-cw)/cw)
+        };
+    }
+
+    //Paint snake
+    function paint() {
+        //Paint the canvas
+        ctx.fillStyle = "black";
+        ctx.fillRect(0,0,w,h);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(0,0,w,h);
+
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+
+        if(d =='right') nx++;
+        else if(d == 'left') nx--;
+        else if(d == 'up') ny--;
+        else if(d == 'down') ny++;
+
+        //Collide code
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)){
+            init();
+
+            return;
         }
+
+        if(nx == food.x && ny == food.y){
+            var tail = {x: nx, y: ny};
+            score++;
+            //Create new food
+            create_food();
+        } else {
+            var tail = snake_array.pop();
+            tail.x = nx; tail.y = ny;
+        }
+
+        snake_array.unshift(tail);
+
+        for(var i = 0;i < snake_array.length;i++){
+            var c = snake_array[i];
+            paint_cell(c.x, c.y);
+        }
+
+        paint_cell(food.x,food.y);
+
+        //Check score
+        checkscore(score);
+    }
+
+    function paint_cell(x,y){
+        ctx.fillStyle=color;
+        ctx.fillRect(x*cw,y*cw,cw,cw);
+        ctx.strokeStyle="white";
+        ctx.strokeRect(x*cw,y*cw,cw,cw);
+    }
+
+    function check_collision(x, y, array){
+        for(var i = 0;i < array.length;i++){
+            if(array[i.x == x && array[i].y ==y])
+            return true;
+        }
+        return false;
     }
 });
