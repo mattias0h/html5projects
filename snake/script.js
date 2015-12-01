07a0d1e1cdb0b8a27a5e182f5a1f3cf4ab97ac7e
mattias0h <hintmattias@gmail.com>
2015-12-01 11:45:09 +0200

    Lecture 24: Snake Game UI - CSS [#108704152]

--- snake/script.js
+++ snake/script.js
@@ -1,3 +1,44 @@
-/**
- * Created by Mattias on 1.12.2015.
- */
+$(document).ready(function () {
+    //Define vars
+   var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext("2d");
+    var w = canvas.width();
+    var h = canvas.height();
+    var cw = 15;
+    var d = "right";
+    var food;
+    var score;
+    var speed = 130;
+
+    //Snake array
+    var snake_array;
+
+    //Initializer
+    function init(){
+        create_snake();
+        create_food();
+        score = 0;
+
+        if(typeof game_loop != "undefined") clearInterval(game_loop);
+        game_loop = setInterval(paint, speed);
+    }
+
+    init(); //Run initializer
+
+    //Snake
+    function create_snake(){
+        var length = 5;
+        snake_array = [];
+        for(var i = length-1;i >= 0;i--){
+            snake_array.push({x: i,y:0});
+        }
+    }
+
+    //Create food
+    function create_food() {
+        food = {
+            x:Math.round(Math.random()*(w-cw)/cw),
+            y:Math.round(Math.random()*(h-cw)/cw);
+        }
+    }
+});
