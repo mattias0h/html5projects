78797a12efee8c5d5fdabb39bcbfe8d4e38d8bbf
mattias0h <hintmattias@gmail.com>
2015-12-08 14:34:31 +0200

    Lecture 31: Create Note Object Part - A [#108704158]

--- stickys/js/script.js
+++ stickys/js/script.js
@@ -1,3 +1,38 @@
 /**
  * Created by Mattias on 8.12.2015.
  */
+var db = null;
+
+if(window.openDatabase) {
+    db = openDatabase("NoteTest", "1.0", "Stickys Database", 10000000);
+    if (!db) {
+        alert("Failed to open database");
+    }
+} else {
+    alert("Failed to open database, make sure your browser supports HTML5 web storage");
+}
+
+function Note() {
+    var self = this;
+
+    var note = document.createElement('div');
+    note.className = 'note';
+    note.addEventListener('mousedown', function (e) {
+        return self.onmousedown(e)
+    }, false);
+    note.addEventListener('click', function() {
+        return self.onNoteClick()
+    }, false);
+    this.note = note;
+
+    var close = document.createElement('div');
+    close.className = 'closebutton';
+    close.addEventListener('click', function (e) {
+        return self.close(e)
+    }, false);
+    note.appendChild(close);
+
+    var edit = createElement('div');
+    edit.className = 'edit';
+    edit.setAttribute('contenteditable', false);
+}
