0eb69146c92f4aa2c2fe739135bd5dbf63859b65
mattias0h <hintmattias@gmail.com>
2015-12-08 16:14:56 +0200

    Lecture 34: Drag and Drop Functions Part - B [#108704158]

--- stickys/js/script.js
+++ stickys/js/script.js
@@ -12,6 +12,10 @@ if(window.openDatabase) {
     alert("Failed to open database, make sure your browser supports HTML5 web storage");
 }
 
+var captured = null;
+var highestZ = 0;
+var highestId = 0;
+
 function Note() {
     var self = this;
 
@@ -150,13 +154,56 @@ Note.prototype = {
         });
     },
 
-    saveAsNew = function() {
+    saveAsNew: function() {
         this.timestamp = new Date.getTime();
 
         var note = this;
         db.transaction(function(tx) {
-            tx.executeSql("INSERT INTO MyStickys (id, note, timestamp, left, top, zindex)");
+            tx.executeSql("INSERT INTO MyStickys (id, note, timestamp, left, top, zindex) VALUES(?, ?, ?, ?, ?, ?)", [note.id, note.text, note.timestamp, note.left, note.top, note.zindex]);
         });
+    },
+
+    onMouseDown: function(e) {
+        capture = this;
+        this.startX = e.clientX - this.note.offsetLeft;
+        this.startY = e.clientY - this.note.offsetRight;
+        this.zIndex = ++highestZ;
+
+        var self = this;
+        if(!("mouseMoveHandler" in this)) {
+            this.mouseMoveHandler = function(e) {return self.onMouseMove(e)}
+            this.mouseUpHandler = function(e) {return self.onMouseUp(e)}
+        }
+
+        document.addEventListener("mousemove", this.mouseMoveHandler, true);
+        document.addEventListener("mouseup", this.mouseUpHandler, true);
+    },
+
+    onMouseMove: function(e) {
+        if(this != captured) {
+            return true;
+        }
+        this.left = e.clientX - this.startX + 'px';
+        this.top = e.clientY - this.startY + 'px';
+        return false;
+    },
+
+    onMouseUp: function(e) {
+        document.removeEventListener("mousemove", this.mouseOverHandler, true);
+        document.removeEventListener("mouseup", this.mouseUpHandler, true);
+
+        this.save();
+        return false;
+    },
+
+    onNoteClick: function(e) {
+        this.editField.focus();
+        getSelection().collapseToEnd();
+    },
+
+    onKeyUp() {
+        this.dirty = true;
+        this.saveSoon();
     }
 }
 
