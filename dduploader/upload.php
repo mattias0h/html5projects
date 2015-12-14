7f15dd9b3f21330a8c1950b92ff1cec6e4bf76ee
mattias0h <hintmattias@gmail.com>
2015-12-14 17:32:40 +0200

    Lecture 41: PHP upload file Part - A [#108704160]

--- dduploader/upload.php
+++ dduploader/upload.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mattias
- * Date: 14.12.2015
- * Time: 13:59
- */
+    $upload_dir = 'uploads/';
+    $allowed_ext = array('jpg','jpeg','png','gif');
+
+    if(array_key_exists('pic',$_FILES) && $_FILES['pic']['error'] == 0) {
+        $pic = $_FILES['pic'];
+        if(!in_array(get_extension($pic['name']),$allowed_ext)) {
+            exit_status('Only '.implode(',',$allowed_ext).' files are allowed');
+        }
+        if(move_uploaded_files($pic['tmp_name'],$upload_dir.$pic['name'])) {
+            exit_status('Your file was uploaded');
+        }
+    }
+
+    //Helpers
+    function exit_status($str) {
+        echo json_encode(array('status'=>$str));
+        exit;
+    }
+?>
