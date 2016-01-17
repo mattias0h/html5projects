c36de3f6824d29dbc951d07f283621c83f659870
mattias0h <hintmattias@gmail.com>
2016-01-17 13:12:44 +0200

    Lecture 69: Add & Get Customers [#108704172]

--- customer_manager/js/main.js
+++ customer_manager/js/main.js
@@ -1,23 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var request = indexedDB.open('customermanager',1);
 
-    request.onupgradeneeded = function(e){
+    request.onupgradeneeded = function (e) {
         var db = e.target.result;
 
-        if(!db.objectStoreNames.contains('customers')){
+        if(!db.objectStoreNames.contains('customers')) {
             var os = db.createObjectStore('customers',{keyPath: "id", autoIncrement:true});
-            //Create Index for Name
+            //Create Index for name
             os.createIndex('name','name',{unique:false});
         }
     }
 
-    request.onsuccess = function(e){
-        console.log('Success: Opened Database...');
+    request.onsuccess = function(e) {
+        console.log('Success: Opened Database');
         db = e.target.result;
-        //showCustomers();
+        //Show Customers
+        showCustomers();
     }
+    request.onerror = function(e) {
+        console.log('Error');
 
-    request.onerror = function(e){
-        console.log('Error: Could Not Open Database...');
     }
 });
+
+function addCustomer() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+
+    var transaction = db.transaction(["customers"],"readwrite");
+    var store = transaction.objectStore("customers");
+
+    var customer = {
+        name: name,
+        email: email
+    }
+
+
+    var request = store.add(customer);
+
+    request.onsuccess = function(e) {
+        window.location.href="index.html";
+    }
+
+    request.onerror = function(e) {
+        alert("Sorry, the customer was not added");
+        console.log('Error', e.target.error.name);
+    }
+}
+
+function showCustomers(e){
+    var transaction = db.transaction(["customers"],"readonly");
+    var store = transaction.objectStore("customers");
+    var index = store.index('name');
+
+    var output = '';
+    index.openCursor().onsuccess = function(e) {
+        var cursor = e.target.result;
+        if(cursor){
+            output += "<tr>";
+            output += "<td>"+cursor.value.id+"<td>";
+            output += "<td><span>"+cursor.value.name+"</span><td>";
+            output += "<td><span>"+cursor.value.email+"</span><td>";
+            output += "<td><a href=''>Delete</a><td>";
+            output += "</tr>";
+            cursor.continue();
+        }
+        $('#customers').html(output);
+    }
+}
